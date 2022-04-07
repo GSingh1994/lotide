18a0496f41f07428b0441e00912bcf135e5ae992
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-07 15:06:22 -0500

    Add a function to get key from an object using callback

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+//testing function
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const test1 = findKey(
+  {
+    'Blue Hill': { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 12
+);
+
+console.log(assertEqual(test1, 'noma'));
