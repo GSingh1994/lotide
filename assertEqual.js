a0c541829dacde70bb25c141c0f1db161c2e5e07
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-11 00:53:39 -0500

    Moved head test code to headTest file

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
--- head.js
+++ head.js
@@ -1,11 +1,6 @@
-const assertEqual = require('./assertEqual');
-
 const head = (array) => {
   const firstElement = array.shift();
   return firstElement;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
