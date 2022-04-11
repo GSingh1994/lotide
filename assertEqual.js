6e0c19adb53edbc2c9236598899d34739ff198c0
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-11 00:46:18 -0500

    Removed test statements

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,3 @@ const assertEqual = (actual, expected) => {
   }
 };
 module.exports = assertEqual;
-// TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('Bootcamp', 'Bootcamp');
-assertEqual(2, 3);
