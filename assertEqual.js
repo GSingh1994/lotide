c4fe8c973426c25f32d4626e67e15a41101c3965
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-11 00:29:04 -0500

    Add module.exports to use assetEqual fn in other files

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,7 @@ const assertEqual = (actual, expected) => {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+module.exports = assertEqual;
 // TEST CODE
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1, 1);
