c3b989009e85fe648152b0dde2c5e66330b79156
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-11 02:35:44 -0500

    Moved middle test code to middle file

--- middle.js
+++ middle.js
@@ -8,41 +8,4 @@ const middle = (array) => {
   }
   return finalArray;
 };
-
-// TEST/ASSERTION FUNCTIONS
-const assertArraysEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
-    }
-  }
-  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// TEST CODE
-console.log(assertArraysEqual(middle([1]), []));
-console.log(eqArrays(middle([1]), []));
-
-console.log(assertArraysEqual(middle([1, 2]), []));
-console.log(eqArrays(middle([1, 2]), []));
-
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(eqArrays(middle([1, 2, 3]), [2]));
-
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
+module.exports = middle;
