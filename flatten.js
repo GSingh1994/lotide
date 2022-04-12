8dee513aa7adfe206078b489773aa85976399713
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-11 19:26:55 -0500

    Refactor flatten to use mocha and chai for assertions

--- flatten.js
+++ flatten.js
@@ -1,27 +1,3 @@
-const assertArraysEqual = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
-    }
-  }
-  return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const flatten = (array) => {
   let finalResult = [];
   //loop outer array
@@ -37,9 +13,5 @@ const flatten = (array) => {
   }
   return finalResult;
 };
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
-console.log(
-  assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
-);
-console.log(eqArrays(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
+
 module.exports = flatten;
