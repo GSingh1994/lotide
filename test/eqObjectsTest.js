c2d19051bc9012c99c1ddc81ec4d598dfe7a9d87
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-12 10:13:44 -0500

    Remove unnecessary code comments

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,20 +1,6 @@
 const assert = require('chai').assert;
 const eqObjects = require('../index').eqObjects;
 
-// const ab = { a: '1', b: '2' };
-// const ba = { b: '2', a: '1' };
-// console.log(assertEqual(eqObjects(ab, ba), true));
-
-// const abc = { a: '1', b: '2', c: '3' };
-// console.log(assertEqual(eqObjects(ab, abc), false));
-
-// const cd = { c: '1', d: ['2', 3] };
-// const dc = { d: ['2', 3], c: '1' };
-// console.log(assertEqual(eqObjects(cd, dc), true));
-
-// const cd2 = { c: '1', d: ['2', 3, 4] };
-// console.log(assertEqual(eqObjects(cd, cd2), false));
-
 describe('#eqObjects', () => {
   it('should return true if both objects are same', () => {
     const ab = { a: '1', b: '2' };
